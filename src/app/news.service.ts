b4817519c24b94dfc2620a4bddc6c1cd38710dc5
Doron Grinzaig <grdoron@gmail.com>
2017-03-18 23:13:23 +0200

    .

--- src/app/news.service.ts
+++ src/app/news.service.ts
@@ -39,20 +39,20 @@ export class NewsService {
     }
 
     getThreads(forum: string): Promise<Thread[]> {
-        return this.http.get('http://rotter.net/gilad/index.php?forum=' + forum).toPromise() 
-            .then(response => response.json() as Thread[])
+        return this.http.get('http://rotter.net/gilad/index.php?forum=' + forum).toPromise()
+            .then(response => response.json().slice(0, 100) as Thread[])
             .catch(this.handleError);
     }
 
-    getComments(forum: string, om : string): Promise<Comment[]> {
+    getComments(forum: string, om: string): Promise<Comment[]> {
         return this.http.get(`http://rotter.net/gilad/index.php?forum=${forum}&om=${om}`).toPromise()
             .then(response => {
                 var comments = response.json().entries as Comment[];
                 for (var i = 0; i < comments.length; i++) {
-                    comments[i].html = comments[i].html.replace(this.wrapImageRegex, '<img src="$1" class="responsive-img" onload="this.className += \' loaded\'"/>');
+                    comments[i].html = comments[i].html.replace(this.wrapImageRegex, `<img src="$1" class="responsive-img" />`);
                     comments[i].html = comments[i].html.replace(this.wrapUrlRegex, '<a rel="nofollow" href="$1" target="_Blank">$1</a>');
                 }
-                 
+
                 return comments;
             })
             .catch(this.handleError);
